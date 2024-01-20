0ea1ff644f0fc3eb769427fd502b54ac13aba4a1
Mohamed Sayed <mohammedamr94@gmail.com>
2024-01-20 17:39:04 +0000

    init

--- compute_mesh_metrics.py
+++ compute_mesh_metrics.py
@@ -3,7 +3,7 @@ import glob
 import time
 
 from visualizers.visualizer_p3d import GroundUpVisualizerP3D as GroundUpVisualizer
-
+import click
 
 def main(sample_path, dataset_root, save_path, scene_name=None):
 
@@ -24,20 +24,21 @@ def main(sample_path, dataset_root, save_path, scene_name=None):
 
     # Run meshing
     offset = (-3.0, 0.0, 4.0)
-    # cam_p_p3d, rendered_image = gup_visualizer.render_scene(image_size=(256, 256), offset=offset)
-    cam_p_p3d = 0
+    cam_p_p3d, rendered_image = gup_visualizer.render_scene(image_size=(1024, 1024), offset=offset)
 
-    gup_visualizer.export_mesh_p3d(mesh_name=mode+'_updated', save_path=save_path, update_face_colors=False)
+    # gup_visualizer.export_mesh_p3d(mesh_name=mode+'_updated', save_path=save_path, update_face_colors=True)
 
     # Save mesh
     if save_mesh: # p3d mesh won't save like this
         filename = os.path.join(save_path, "mesh_{}_elevation_{}.ply".format(mode ,gup_visualizer.sample_idx))
         gup_visualizer.mesh.export(filename, file_type='ply')
 
-    # filename = os.path.join(save_path, "image_{}_{}.png".format(mode, gup_visualizer.sample_idx))
-    # rendered_image.save(filename, 'PNG')
+    filename = os.path.join(save_path, "image_{}_{}.png".format(mode, gup_visualizer.sample_idx))
+    rendered_image.save(filename, 'PNG')
 
     return gup_visualizer, cam_p_p3d, rendered_image
+
+
 
 """
 1) read gt, proj, pred, sketch_td, sketch_p, cam_td, cam_p
@@ -46,23 +47,47 @@ def main(sample_path, dataset_root, save_path, scene_name=None):
 4) pc proj
 5) Render from cam_p
 """
-if __name__ == '__main__':
-
+@click.command()
+@click.option(
+    "--dataset_path",
+    type=str,
+    default="/mnt/data_s/gunlu/data_gen/UrbanScene3D",
+    help="Root directory of datasets",
+)
+@click.option(
+    "--dataset_name",
+    type=str,
+    default="sf_test_1k_2",
+    help="dataset name",
+)
+@click.option(
+    "--predictions_path",
+    type=str,
+    default="/mnt/data_s/gunlu/data_gen/UrbanScene3D/sf_test_1k_2",
+    help="Root directory of predictions",
+)
+@click.option(
+    "--model_name",
+    type=str,
+    default="test231203_000446_v14_guide_gt_pt_rn_pt_vae_l1l2norm_max5.2_lr3",
+    help="Model name",
+)
+@click.option(
+    "--save_path",
+    type=str,
+    help="Save path.",
+)
+def cli(dataset_path: str, dataset_name: str, predictions_path: str, model_name: str, save_path: str):
     # data path
-    data_path = '/mnt/data_f/gunlu/Experiments/GroundUp/Diffusion/diff_results_v14'
+    # data_path = '/home/Projects/groundup_visualizer_local/data'
 
     # Get paths
-    model_name = 'test231203_000446_v14_guide_gt_pt_rn_pt_vae_l1l2norm_max5.2_lr3' #'test231202_235909_v14_guide_gt_pt_rn_pt_vae_l1l2_max5.2_lr3' #'test231203_000446_v14_guide_gt_pt_rn_pt_vae_l1l2norm_max5.2_lr3' #'v15_no_guide_pt_l1l2norm_max5.2_lr1'
-    path_to_samples = os.path.join(data_path, 'models', model_name)
-    samples = sorted(glob.glob(os.path.join(path_to_samples, '*.npy')))
-
-    dataset_name =  'sf_test_1k_2' #'sf_pilot_demo_delete_3' # 'sf_test_1k_2'
-    dataset_root_path = os.path.join(data_path, dataset_name)
-
-    # Save path
-    model_name = model_name + '_DEBUG'
-
-    save_path = os.path.join(data_path, 'models', model_name, "viz")
+    # model_name = 'test231203_000446_v14_guide_gt_pt_rn_pt_vae_l1l2norm_max5.2_lr3' #'test231202_235909_v14_guide_gt_pt_rn_pt_vae_l1l2_max5.2_lr3' #'test231203_000446_v14_guide_gt_pt_rn_pt_vae_l1l2norm_max5.2_lr3' #'v15_no_guide_pt_l1l2norm_max5.2_lr1'
+    # path_to_samples = os.path.join(data_path, 'models', model_name)
+    
+    print(predictions_path)
+    samples = sorted(glob.glob(os.path.join(predictions_path, '*.npy')))
+    
     if not os.path.exists(save_path):
         os.makedirs(save_path, exist_ok=True)
 
@@ -73,7 +98,7 @@ if __name__ == '__main__':
         start_time = time.time()
         # Run visualization
         gup_viz, cam_p, rendered_image = main(sample,
-                                              dataset_root=dataset_root_path,
+                                              dataset_root=os.path.join(dataset_path, dataset_name),
                                               save_path=save_path,
                                               scene_name=dataset_name)
         # End timer
@@ -85,3 +110,7 @@ if __name__ == '__main__':
         # plotly_vis.plot_batch_individually([gup_viz.mesh, cam_p])
 
     # cam_p_p3d, rendered_image = gup_viz.render_scene(image_size=(512, 512), )
+
+
+if __name__ == '__main__':
+    cli()
